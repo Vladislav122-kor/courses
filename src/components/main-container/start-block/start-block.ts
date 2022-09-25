0e6823952e1a10880e88f7250a4469882f39bf9a
Vladislav Korolyov <vladislav.korolyov1996@gmail.com>
2022-09-26 00:51:51 +0300

    feat: add adaptation for start-block

--- src/components/main-container/start-block/start-block.ts
+++ src/components/main-container/start-block/start-block.ts
@@ -1,27 +1,40 @@
 import Component from '../../../utils/component';
 
-import './index.scss';
+import './start-block.scss';
 
 class StartBlock extends Component {
-    private startBlockContainer: Component;
+  private container: Component;
 
-    private textBlock: Component;
+  private textBlock: Component;
 
-    private title: Component;
+  private text: Component;
 
-    private description: Component;
+  private title: Component;
 
-    private button: Component;
+  private description: Component;
+
+  private button: Component;
     
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', ['start-block']);
+
+    // create background image for this block
     this.element.style.backgroundImage = 'linear-gradient( rgba(25, 25, 26, 0.5), rgba(25, 25, 26, 0.5) ), url("./assets/img/start-image.jpg")';
 
-    this.startBlockContainer = new Component(this.element, 'div', ['start-block__container']);
-    this.textBlock = new Component(this.startBlockContainer.element, 'div', ['start-block__text']);
-    this.title = new Component(this.textBlock.element, 'h1', ['start-block__text-title'], 'Тренинги Различных Направлений');
-    this.description = new Component(this.textBlock.element, 'p', ['start-block__text-description'], 'Учебно-консультационный центр «Магнат» более 10-и лет предоставляет обучающие услуги в сфере обслуживания, ремонта автомобилей и управления автосервисным предприятием. В разделе “Тренинги” Вы можете выбрать познакомиться с перечнем и содержанием проводимых тренингов, а также записаться на обучение. Обучаемся Вместе с удовольствием!');
-    this.button = new Component(this.textBlock.element, 'a', ['start-block__text-button'], 'ВЫБРАТЬ ТРЕНИНГ');
+    this.container = new Component(this.element, 'div', ['start-block__container']);
+
+    // create container with all information
+    this.textBlock = new Component(this.container.element, 'div', ['start-block__text-block']);
+
+    // create text part
+    this.text = new Component(this.textBlock.element, 'div', ['start-block__text-block__text']);
+    this.title = new Component(this.text.element, 'h1', ['start-block__text-block__text__title']);
+    this.title.element.innerHTML = 'Тренинги<br>различных направлений'
+    this.description = new Component(this.text.element, 'p', ['start-block__text-block__text__description'], 'Учебно-консультационный центр «Магнат» более 10-и лет предоставляет обучающие услуги в сфере обслуживания, ремонта автомобилей и управления автосервисным предприятием. В разделе “Тренинги” Вы можете ознакомиться с перечнем и содержанием проводимых тренингов, а также записаться на обучение.');
+    this.description.element.innerHTML += '<br>Обучаемся Вместе с удовольствием!';
+
+    // create button with link to all trainings page
+    this.button = new Component(this.textBlock.element, 'a', ['start-block__text-block__button'], 'ВЫБРАТЬ ТРЕНИНГ');
     this.button.element.setAttribute('href', '#/trainings');
   }
 
